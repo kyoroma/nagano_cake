8a3c17a1e5c513dba06a5b37becaba5b824e2f3e
kyoroma <138451440+kyoroma@users.noreply.github.com>
2023-09-14 14:20:59 +0000

    [Update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+
+  root to: 'homes#top'
+
+  devise_for :customers,skip: [:passwords], controllers: {
+    sessions: 'public/sessions',
+    registrations: 'public/registrations'
+  }
+
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: 'admin/sessions'
+  }
+
+  namespace :admin do
+    resources :items
+  end
+
+  namespace :customer do
+    resources :items
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
