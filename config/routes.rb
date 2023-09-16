d367a6697dc887a41664e6087669d0f0d60e6210
kyoroma <138451440+kyoroma@users.noreply.github.com>
2023-09-16 15:09:15 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
 
-  root to: 'homes#top'
-
-  devise_for :customers,skip: [:passwords], controllers: {
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+    get '/about', to: 'static_pages#about', as: 'about'
+  end
+  devise_for :customers, skip: [:passwords], controllers: {
     sessions: 'public/sessions',
     registrations: 'public/registrations'
   }
@@ -12,12 +15,17 @@ Rails.application.routes.draw do
   }
 
   namespace :admin do
+    root to: 'homes#top'
     resources :items
+    resources :orders, only: [:show]
+    resources :customers, only: [:index, :show, :edit]
+    delete '/sign_out', to: 'sessions#destroy', as: :destroy_admin_session
   end
 
   namespace :customer do
     resources :items
   end
 
+  root to: 'admin/homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
