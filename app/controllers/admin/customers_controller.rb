65b36a8e47a459026e042bf7ccff33ca631cffaf
kyoroma <138451440+kyoroma@users.noreply.github.com>
2023-10-01 08:19:14 +0000

    [Finish]会員一覧完成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,28 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page]).per(10)
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer), notice: '顧客情報を更新しました。'
+    else
+      render :edit
+    end
+  end
+  
+  private
+
+  def customer_params
+    params.require(:customer).permit(:name, :email, :other_attributes)
   end
 end
